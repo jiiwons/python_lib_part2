59392cec81f0ff1186aa3bab1939dc819a374d13
jiiwons <sjiwon0402@naver.com>
2025-03-17 10:02:29 +0900

    quiz2 강사님 코드 추가

--- day5/quiz2.py
+++ day5/quiz2.py
@@ -6,21 +6,29 @@ plt.rc('font', family='Malgun Gothic')
 df = pd.read_csv('unemployment_rate.csv', encoding='cp949', index_col='연도')
 print(df)
 
-data2001 = df.iloc[0]
-data2002 = df.iloc[1]
-data2003 = df.iloc[2]
-data2004 = df.iloc[3]
-data2005 = df.iloc[4]
-print(data2001)
-plt.figure(figsize=(12,8))
-plt.plot(data2001, color='blue', marker='o', label='2001년')
-plt.plot(data2002, color='orange', marker='o', label='2002년')
-plt.plot(data2003, color='green', marker='o', label='2003년')
-plt.plot(data2004, color='red', marker='o', label='2004년')
-plt.plot(data2005, color='purple', marker='o', label='2005년')
+# data2001 = df.iloc[0]
+# data2002 = df.iloc[1]
+# data2003 = df.iloc[2]
+# data2004 = df.iloc[3]
+# data2005 = df.iloc[4]
+# print(data2001)
+# plt.figure(figsize=(12,8))
+# plt.plot(data2001, color='blue', marker='o', label='2001년')
+# plt.plot(data2002, color='orange', marker='o', label='2002년')
+# plt.plot(data2003, color='green', marker='o', label='2003년')
+# plt.plot(data2004, color='red', marker='o', label='2004년')
+# plt.plot(data2005, color='purple', marker='o', label='2005년')
+#
+# plt.xlabel('연령대')
+# plt.ylabel('연도명')
+# plt.title('연령대별 연도명 꺾은 선')
+# plt.legend(loc=1)
+# plt.show()
+chartdata = df.loc['2001년':'2005년']
 
+chartdata = chartdata.T
+chartdata.plot(title='연령대별 연도명 꺾은 선', figsize=(8,5), legend=True, marker='o', rot=0)
 plt.xlabel('연령대')
 plt.ylabel('연도명')
-plt.title('연령대별 연도명 꺾은 선')
-plt.legend(loc=1)
+
 plt.show()
