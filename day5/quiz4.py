c6d6ee49b85b154ada7432e389431c2f89e23176
jiiwons <sjiwon0402@naver.com>
2025-03-17 09:38:43 +0900

    quiz4 changed

--- day5/quiz4.py
+++ day5/quiz4.py
@@ -53,18 +53,45 @@ print(total_mean)
 print(data[data['total_litres_of_pure_alcohol'] > total_mean]['continent'].value_counts())
 
 #  ㅅ. 대륙별 spirit_servings의 평균, 최소, 최대, 합계를 막대 그래프로 출력
-cont_mean = data.groupby('continent')['spirit_servings'].mean()
-cont_min = data.groupby('continent')['spirit_servings'].min()
-cont_max = data.groupby('continent')['spirit_servings'].max()
-cont_sum = data.groupby('continent')['spirit_servings'].sum()
+# cont_mean = data.groupby('continent')['spirit_servings'].mean()
+# cont_min = data.groupby('continent')['spirit_servings'].min()
+# cont_max = data.groupby('continent')['spirit_servings'].max()
+# cont_sum = data.groupby('continent')['spirit_servings'].sum()
 
-cont = data.groupby("continent")["spirit_servings"].agg(["mean","min","max","sum"])
-index = cont_mean.index
+cont = data.groupby("continent")["spirit_servings"].agg(["mean","min","max","sum"]).reset_index()
+
+fig, ax = plt.subplots(figsize=(12,6))
+
+index = np.arange(len(cont['continent']))
 bar_width = 0.25
-plt.bar(index, cont_mean, color='red')
-plt.bar(index+bar_width, cont_min, color='green')
-plt.bar(index+2*bar_width, cont_max)
-plt.bar(index+3*bar_width, cont_sum)
+ax.bar(index, cont['mean'], bar_width, label='Mean', color='red')
+ax.bar(index + bar_width, cont['min'], bar_width, label='Min', color='green')
+ax.bar(index + 2 * bar_width, cont['max'], bar_width, label='Max', color='blue')
+ax.bar(index + 3 * bar_width, cont['sum'], bar_width, label='Sum', color='gold')
+
+ax.set_xticks(index + 1.5 * bar_width)
+ax.set_xticklabels(cont['continent'])
+ax.grid(True)
+ax.legend()
+
 plt.show()
+
 #  ㅇ. 대륙별 total_litres_of_pure_alcohol를 막대 그래프로 출력
+cont_alchol = data.groupby('continent')['total_litres_of_pure_alcohol'].mean().reset_index()
+
+mean = data['total_litres_of_pure_alcohol'].mean()
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+ax.bar(cont_alchol['continent'], cont_alchol['total_litres_of_pure_alcohol'], color='skyblue', alpha=0.7, label='Continent')
+ax.bar('mean', mean, color='red', alpha=0.7, label='Mean')
+ax.axhline(mean, color='black', linestyle='--')
+
+ax.set_xlabel('Continent')
+ax.set_ylabel('total_litres_of_pure_alcohol')
+ax.set_title('total_litres_of_pure_alcohol by Continent')
+ax.grid(True)
+ax.legend()
+
+plt.show()
 
